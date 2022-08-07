61bdd5b38872c20dd05d07e6d845dbc1ed924707
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-08-07 16:26:46 +0100

    Adding Comments

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from datetime import date
 import numpy as np
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -53,7 +55,7 @@ def precipitation():
 # Previous Year 
     prv_year = "2016-08-23"
 
-    """Return a list of all precipitation """
+    """Return a JSON list of all precipitation from the dataset"""
     # Query all pprecipitation
     prcp_data = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prv_year).order_by(Measurement.date).all()
@@ -76,6 +78,8 @@ def precipitation():
     return jsonify(all_precipitation)
 
 
+"""Return a JSON list of all stations from the dataset"""
+
 @app.route("/api/v1.0/stations")
 def station():
     station_data = session.query(Station.station, Station.name).all()
@@ -83,8 +87,10 @@ def station():
     print(stations)
     return jsonify(stations)
 
-@app.route("/api/v1.0/tobs")
 
+"""Return a JSON list of all temperature observations (TOBS) for the previous year"""
+
+@app.route("/api/v1.0/tobs")
 def tobs():
     prv_year = "2016-08-23"
     tobs_data = session.query(Measurement.tobs).\
@@ -95,12 +101,35 @@ def tobs():
     print(tobs)
     return jsonify(tobs)
 
+"""Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a start range """
 
 @app.route("/api/v1.0/<start>")
-def StartDateOnly():
-    startdate_data = session.query()
+def startDate(start):
 
+    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
+    end =  dt.date(2016, 12, 31)
+    startdate_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
 
+    startdate = list(np.ravel(startdate_data))
+    print(startdate)
+    return jsonify(startdate)
+
+"""Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for start-end range"""  
+
+@app.route("/api/v1.0/<start>/<end>")
+def StartDateEndDate(start,end):
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date = dt.datetime.strptime(end, '%Y-%m-%d')
+    end =  dt.date(2016, 12, 31)
+    start = dt.date(2015, 1, 1)
+    
+    startdate_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    startdate = list(np.ravel(startdate_data))
+    print(startdate)
+    return jsonify(startdate)
 
 
 
